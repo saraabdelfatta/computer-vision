80971cee591c6657d5cbdb14c628cd166bc101f2
saraabdelfatta <112962589+saraabdelfatta@users.noreply.github.com>
2025-03-22 23:17:15 +0200

    Add files via upload

---
+++ ass2/Part 2_pyramids/runThis.m
@@ -0,0 +1,27 @@
+function runThis
+
+N = 5;
+% Load an image
+[G, L] = pyramidsGL(im, N)
+
+% Display the Gaussian and Laplacian pyramid
+displayPyramids(G, L);
+
+end
+
+function [G, L] = pyramidsGL(im, N)
+% [G, L] = pyramidsGL(im, N)
+% Creates Gaussian (G) and Laplacian (L) pyramids of level N from image im.
+% G and L are cell where G{i}, L{i} stores the i-th level of Gaussian and Laplacian pyramid, respectively. 
+
+end
+
+function displayPyramids(G, L)
+% Displays intensity and fft images of pyramids
+
+end
+
+function displayFFT(im, minv, maxv)
+% Displays FFT images
+
+end
